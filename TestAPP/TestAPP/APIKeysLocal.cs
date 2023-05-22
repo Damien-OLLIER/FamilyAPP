2ed4bf16e3d4a3a5a268168ce98a402025ea0d9d
Damien-OLLIER <71604541+Damien-OLLIER@users.noreply.github.com>
2023-05-22 23:07:29 +0200

    key prob resolution

---
+++ TestAPP/TestAPP/APIKeysLocal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestAPP
+{
+    public static partial class APIKeys
+    {
+        static APIKeys()
+        {
+            //accessToken = "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";
+            //Approval = true;
+        }
+    }    
+}
